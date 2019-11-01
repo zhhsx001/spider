c5803068b0f58b7c56298148be55fb9e867311ee
zhanghua <zhhsx001@163.com>
2019-11-01 17:39:46 +0800

    base over

--- new.py
+++
@@ -1,8 +0,0 @@
-from bs4 import BeautifulSoup
-import requests
-
-
-url = 'http://47.94.140.188:18035/#/article/create_article'
-res = requests.get(url)
-with open('new.html', 'w', encoding='utf8') as f:
-    f.write(res.text)
