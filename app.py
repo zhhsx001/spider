d2df4dfd4fd5c371921b28fdbb2f7f5e2f0b7317
zhanghua <zhhsx001@163.com>
2019-11-04 19:13:54 +0800

    sss

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
+import os
 from flask import Flask, url_for, render_template, request, flash, redirect
+from bs4 import BeautifulSoup
 import pandas as pd
-
+import spider
+import article_handle
 app = Flask(__name__)
 
 
@@ -17,13 +20,56 @@ def show_title():
 def accept_title():
     result = []
     for key in request.form.keys():
-        print(key)
+        # print(key)
         result.append(key)
     result = pd.Series(result)
-    print(result)
+    # print(result)
     result.to_csv('choices.csv', header=None, encoding='utf8')
-    return 'submitting '
+    return render_template('complete.html')
+
+
+@app.route('/report', methods=['GET', 'POST'])
+def report():
+
+    spider.big_and_small_spilt()
+    big_html = os.listdir('new/big')
+    small_html = os.listdir('new/small')
+    df = pd.read_csv('df_test.csv')
+    print('the last number: ', df.count()[0])
+    for i in range(30):
+
+        # 处理比例不平衡条件。
+        if i % 3 == 0 and big_html:
+            html_uri = big_html.pop()
+            img_uri = 'new/big/'+html_uri[:-5]+'p.'
+            button = True
+            html_uri = 'new/big/'+ html_uri
+        elif small_html:
+            html_uri = small_html.pop()
+            img_uri = 'new/small/'+html_uri[:-5]+'p.'
+            button = False
+            html_uri = 'new/small/'+html_uri
+        #print(html_uri)
+        with open(html_uri, 'r', encoding='utf8') as f:
+            html_page = f.read()
+        print(html_uri)
+        article = article_handle.Article(html_page, html_uri[:-5].split('/')[-1])
+        article.all_handle()
+
+        # title, image_url = [], button = False, content = '', video = ''
+        title = df.iloc[int(html_uri[:-5].split('/')[-1])]['title']
+        image_url = [{'url': 'https://cdn.kingmasports.com/'+spider.picture_post(img_uri+article.img_kind,
+                                                                                  article.img_kind)}]
+        print(image_url)
+        content = article.content.prettify()
+        video = ''
+        spider.create_article(title=title, image_url=image_url, button=button, content=content, video=video)
+
+    return render_template('report.html')
 
 
 if __name__ == '__main__':
+    # url_ = 'https://news.163.com/'
+    # file_name = 'df_test'
+    # spider.home_spider(url_, file_name)
     app.run()
---
+++ article_handle.py
@@ -0,0 +1,88 @@
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+
+
+class Article(object):
+    def __init__(self, html, file_id, kind=None):
+        # content: BeautifulSoup.
+        self.img_kind = ''
+        self.id = file_id
+        self.kind = kind
+        self.html = BeautifulSoup(html, 'lxml')
+        self.content = self.html.body.find(class_="post_content post_area clearfix").find(id="epContentLeft").\
+            find(class_="post_body").find(id="endText")
+
+    def content_kind(self):
+        # 不显示图集类型文章。短文字，加视频的文章如何？
+        pass
+
+    def old_title(self):
+        # 删除原标题
+        tap = self.content.p
+        if tap.get('class')[0] == 'otitle':
+            tap.extract()
+
+    def video(self):
+        # 删除视频
+        for tap in self.content.find_all(class_='video-inner'):
+            tap.extract()
+
+    def del_link(self):
+        # 删除扩展阅读
+        tap = self.content.find(class_='special_tag_wrap')
+        if tap:
+            tap.extract()
+
+    def del_editor(self):
+        # 删除文脚编辑，文章内容编辑。
+        tap = self.content.find(class_='ep-source cDGray')
+        tap.extract()
+
+    def add_editor(self):
+        # 定位文章内容，删除页脚责编，增加诸黎。
+        p_list = self.content.find_all('p')
+        p_list[-1].string = '编辑诸黎'
+
+    def del_blank(self):
+        # 删除空行
+        pass
+
+    def save_img(self):
+        tap_img = self.content.img
+        if tap_img:
+            img_url = tap_img.get('src')
+            img = requests.get(img_url)
+            with open('new/big/'+self.id+'p.'+img_url.split('.')[-1].split('?')[0], 'wb') as f:
+                f.write(img.content)
+        else:
+            print(self.id)
+            img_url = pd.read_csv('df_test.csv').iloc[int(self.id)]['img']
+            img = requests.get(img_url)
+            with open('new/small/'+self.id+'p.'+img_url.split('.')[-1].split('?')[0], 'wb') as f:
+                f.write(img.content)
+        self.img_kind = img_url.split('.')[-1].split('?')[0]
+
+    def tech_handle(self):
+        # 科技类文章，头尾需要特殊处理。
+        p_list = self.content.find_all('p')
+        if p_list[2].b:
+            p_list[2].extract()
+        if p_list[3].b:
+            p_list[3].extract()
+
+    def all_handle(self):
+        # 所有处理，返回最终content图文结果
+        self.old_title()
+        self.video()
+        self.del_link()
+        self.del_editor()
+        self.add_editor()
+        self.save_img()
+        if self.kind == 'tech':
+            self.tech_handle()
+
+        return self.content.prettify()
+
+
+
